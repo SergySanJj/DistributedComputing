b193561c025172180993e458139c72e13369af95
Sergei Yarema <isara.isara8@gmail.com>
2020-02-19 23:19:19 +0200

    Add part 2

---
+++ Lab4/part2/src/main/java/com/sergeiyarema/Plant.java
@@ -0,0 +1,19 @@
+package com.sergeiyarema;
+
+public class Plant {
+    private boolean state = false;
+
+    public synchronized void setState(boolean state) {
+        this.state = state;
+    }
+
+    public boolean getState() {
+        return state;
+    }
+
+    @Override
+    public synchronized String toString() {
+        if (state) return "o";
+        else return "x";
+    }
+}
