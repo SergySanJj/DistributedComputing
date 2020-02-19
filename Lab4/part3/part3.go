9934e1900524319e5a6f349ea18af690044cb421
Sergei Yarema <isara.isara8@gmail.com>
2020-02-20 00:13:46 +0200

    Refactor graph

--- Lab4/part3/part3.go
+++ Lab4/part3/part3.go
@@ -9,21 +9,8 @@ import (
 )
 
 type Edge struct {
-	to int
+	to    int
 	price int
-}
-
-func main() {
-	N := rand.Intn(15) + 5
-	graph := generateGraph(N)
-	mutex := sync.RWMutex{}
-	quit := make(chan bool)
-
-	go changePrice(graph, mutex)
-	go changeRides(graph, mutex)
-	go changeTowns(graph, mutex)
-	go showPathCost(graph, mutex)
-	<- quit
 }
 
 func generateGraph(N int) [][]Edge {
@@ -50,9 +37,7 @@ func generateGraph(N int) [][]Edge {
 			}
 		}
 	}
-	for i := 0; i < N; i++ {
-		fmt.Println(graph[i])
-	}
+	printGraph(graph)
 	return graph
 }
 
@@ -61,46 +46,42 @@ func ridePrice() int {
 }
 
 func getNumberOfTowns(graphLength int) int {
-	return int(math.Sqrt(float64(1 + 8 * graphLength)) - 1) / 2 + 1
+	return int(math.Sqrt(float64(1+8*graphLength))-1)/2 + 1
 }
 
-func changePrice(graph [][]Edge, mutex sync.RWMutex) {
+func changePrice(graph [][]Edge, readWriteLock sync.RWMutex) {
 	for {
-		mutex.RLock()
+		readWriteLock.RLock()
 		N := len(graph)
 		if N != 0 {
 			i := rand.Intn(N)
 			if len(graph[i]) != 0 {
 				j := rand.Intn(len(graph[i]))
 
-				fmt.Print("Price at [", i, ", ", graph[i][j].to, "] has changed from ", graph[i][j].price, " to ")
-				// if price is 0, then there is no path, so we make it always
-				// nonzero in order to prevent path deleting, such function
-				// is done by another thread
 				graph[i][j].price = ridePrice() + 1
-				fmt.Println(graph[i][j].price)
+				fmt.Println("Update ", i, " -> ", graph[i][j].to, "     price ", graph[i][j].price)
 			}
 		}
-		mutex.RUnlock()
-		time.Sleep(time.Second * time.Duration(rand.Intn(10) + 1))
+		readWriteLock.RUnlock()
+		time.Sleep(time.Second * time.Duration(rand.Intn(10)+1))
 	}
 }
 
-func changeRides(graph [][]Edge, mutex sync.RWMutex)  {
+func changeRides(graph [][]Edge, readWriteLock sync.RWMutex) {
 	for {
-		mutex.Lock()
+		readWriteLock.Lock()
 		N := len(graph)
 		if rand.Intn(2) == 0 {
-			graph = addRide(graph, N)
+			graph = generateNewPath(graph, N)
 		} else {
 			graph = removeRide(graph, N)
 		}
-		mutex.Unlock()
-		time.Sleep(time.Second *time.Duration(rand.Intn(10) + 1))
+		readWriteLock.Unlock()
+		time.Sleep(time.Second * time.Duration(rand.Intn(10)+1))
 	}
 }
 
-func addRide(graph [][]Edge, N int) [][]Edge {
+func generateNewPath(graph [][]Edge, N int) [][]Edge {
 	if N == 0 {
 		return graph
 	}
@@ -118,7 +99,7 @@ func addRide(graph [][]Edge, N int) [][]Edge {
 		if !visited[j] {
 			price := ridePrice() + 1
 			graph[i] = append(graph[i], Edge{j, price})
-			fmt.Println("New path was created from ", i, " to ", j, " with price ", price)
+			fmt.Println("New path ", i, " -> ", j, "      price ", price)
 			break
 		}
 	}
@@ -131,7 +112,7 @@ func removeRide(graph [][]Edge, N int) [][]Edge {
 		i := rand.Intn(N)
 		if len(graph[i]) != 0 {
 			j := rand.Intn(len(graph[i]))
-			fmt.Println("Path was removed from ", i, " to ", graph[i][j].to)
+			fmt.Println("Remove path ", i, " -> ", graph[i][j].to)
 			graph[i] = append(graph[i][:j], graph[i][j+1:]...)
 		}
 	}
@@ -139,7 +120,7 @@ func removeRide(graph [][]Edge, N int) [][]Edge {
 }
 
 func changeTowns(graph [][]Edge, mutex sync.RWMutex) {
-	for  {
+	for {
 		mutex.Lock()
 		if rand.Intn(2) == 0 {
 			graph = addTown(graph)
@@ -147,96 +128,106 @@ func changeTowns(graph [][]Edge, mutex sync.RWMutex) {
 			graph = removeTown(graph)
 		}
 		mutex.Unlock()
-		time.Sleep(time.Second * time.Duration(rand.Intn(10) + 1))
+		time.Sleep(time.Second * time.Duration(rand.Intn(10)+1))
 	}
 }
 
 func addTown(graph [][]Edge) [][]Edge {
 	newTown := make([]Edge, 0)
 	graph = append(graph, newTown)
-	fmt.Println("New town ", len(graph) - 1, " was added")
+	fmt.Println("New town ", len(graph)-1)
 	return graph
 }
 
 func removeTown(graph [][]Edge) [][]Edge {
 	N := len(graph)
 	town := rand.Intn(N)
-	graph = append(graph[:town], graph[town + 1:]...)
-	for i := 0; i < N - 1; i++ {
+	graph = append(graph[:town], graph[town+1:]...)
+	for i := 0; i < N-1; i++ {
 		for j := 0; j < len(graph[i]); j++ {
 			if graph[i][j].to == town {
-				graph[i] = append(graph[i][:j], graph[i][j + 1:]...)
+				graph[i] = append(graph[i][:j], graph[i][j+1:]...)
 			} else if graph[i][j].to > town {
 				graph[i][j].to--
 			}
 		}
 	}
-	fmt.Println("Town ", town, " was deleted")
+	fmt.Println("Remove town ", town)
 	fmt.Println(graph)
 	return graph
 }
 
-func showPathCost(graph [][]Edge, mutex sync.RWMutex) {
-	for  {
+func pathPrice(graph [][]Edge, mutex sync.RWMutex) {
+	for {
 		mutex.RLock()
 		N := getNumberOfTowns(len(graph))
 		i := rand.Intn(N)
 		j := rand.Intn(N)
 		if i != j {
-			cost := findPath(graph, i, j)
-			if cost == 0 {
-				fmt.Println("Where is no path from ", i, " to ", j)
+			price := findPath(graph, i, j)
+			if price == 0 {
+				fmt.Println("No path ", i, " -> ", j)
 			} else {
-				fmt.Println("Ride price from town ", i, " to ", j, " is ", cost)
+				fmt.Println(i, " -> ", j, " price: ", price)
 			}
 		}
 		mutex.RUnlock()
-		time.Sleep(time.Second * time.Duration(rand.Intn(10) + 1))
+		time.Sleep(time.Second * time.Duration(rand.Intn(10)+1))
 	}
 }
 
 func findPath(graph [][]Edge, from int, to int) int {
 	INF := math.MaxInt64
 	N := len(graph)
-	visited := make([] bool, N)
-	distances := make([]int, N)
+	visited := make([]bool, N)
+	prices := make([]int, N)
 	for i := 0; i < N; i++ {
 		visited[i] = false
-		distances[i] = INF
+		prices[i] = INF
 	}
 
-	distances[from] = 0
+	prices[from] = 0
 
 	for i := 0; i < N; i++ {
 		v := -1
 		for j := 0; j < N; j++ {
-			if !visited[j] && (v == -1 || distances[j] < distances[v]) {
+			if !visited[j] && (v == -1 || prices[j] < prices[v]) {
 				v = j
 			}
 		}
-		if distances[v] == INF {
+		if prices[v] == INF {
 			break
 		}
 		visited[v] = true
 		for j := 0; j < len(graph[i]); j++ {
 			to := graph[i][j].to
-			len := graph[i][j].price
-			if distances[v] + len <distances[to] {
-				distances[to] = distances[v] + len
+			price := graph[i][j].price
+			if prices[v]+price < prices[to] {
+				prices[to] = prices[v] + price
 			}
 		}
 	}
-	if distances[to] == INF {
+	if prices[to] == INF {
 		return 0
 	}
-	return distances[to]
+	return prices[to]
 }
 
-
-func printGraph(graph [][]Edge)  {
-	N := len(graph)
-	for i := 0; i < N; i++ {
-		fmt.Println(graph[i])
+func printGraph(graph [][]Edge) {
+	for i := 0; i < len(graph); i++ {
+		fmt.Println(i, ": ", graph[i])
 	}
 }
 
+func main() {
+	N := rand.Intn(8) + 5
+	graph := generateGraph(N)
+	readWriteLock := sync.RWMutex{}
+	quit := make(chan bool)
+
+	go changePrice(graph, readWriteLock)
+	go changeRides(graph, readWriteLock)
+	go changeTowns(graph, readWriteLock)
+	go pathPrice(graph, readWriteLock)
+	<-quit
+}
