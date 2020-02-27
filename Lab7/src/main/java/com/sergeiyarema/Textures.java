04c0111e42f0f9c22408915c887ef79c674b6202
Sergei Yarema <isara.isara8@gmail.com>
2020-02-27 17:37:06 +0200

    Add basic duck hunt game

---
+++ Lab7/src/main/java/com/sergeiyarema/Textures.java
@@ -0,0 +1,18 @@
+package com.sergeiyarema;
+
+import javax.swing.*;
+
+public class Textures {
+    public final static String BACKGROUND = "images/background.png";
+
+    public final static String BULLET = "images/bullet.png";
+
+    public final static String LDUCK = "images/DuckLR.png";
+    public final static String RDUCK = "images/DuckRL.png";
+
+    public final static String RHUNTER = "images/hunterRL.png";
+    public final static String LHUNTER = "images/hunterRL.png";
+
+    public final static String SIGHT = "images/sight.png";
+
+}
